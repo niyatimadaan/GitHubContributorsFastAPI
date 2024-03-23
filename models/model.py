e7bd5f757b8bf5cdf9db5fbee7a63abfd45f8696
niyatimadaan <madaanniyati@gmail.com>
2024-03-23 11:49:08 +0530

    adding pattern

--- models/model.py
+++ models/model.py
@@ -1,13 +1,13 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Repository(BaseModel):
-    owner: str
+    owner: str = Field(pattern=r"^[a-zA-Z0-9-]+$")
     repo: str
 
 class ContributorInfo(BaseModel):
-    owner: str
+    owner: str = Field(pattern=r"^[a-zA-Z0-9-]+$")
     repo: str
     username: str
     type: str
